a0469dfd0a85b324a5e4e2fb2e8a80c9a2db3637
memsharded <james@conan.io>
2016-07-24 19:42:51 +0200

    converted to header only

--- conanfile.py
+++ conanfile.py
@@ -7,35 +7,18 @@ class FmtConan(ConanFile):
     version = "3.0.0"
     license = "BSD"
     url = "https://github.com/memsharded/conan-fmt"
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
-    generators = "cmake"
     build_policy = "missing"
 
     def source(self):
        self.run("git clone https://github.com/fmtlib/fmt")
        self.run("cd fmt && git checkout 3.0.0")
-       # This small hack might be useful to guarantee proper /MT /MD linkage in MSVC
-       # if the packaged project doesn't have variables to set it properly
-       tools.replace_in_file("fmt/CMakeLists.txt", "project(FMT)", '''project(FMT)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
-
-    def build(self):
-        cmake = CMake(self.settings)
-        shared = "-DBUILD_SHARED_LIBS=ON" if self.options.shared else ""
-        shared += " -DFMT_TEST=OFF -DFMT_INSTALL=OFF -DFMT_DOCS=OFF"
-        self.run('cmake fmt %s %s' % (cmake.command_line, shared))
-        self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
         self.copy("*.h", dst="include/fmt", src="fmt/fmt")
         self.copy("*.h", dst="include/cppformat", src="cppformat")
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("*.cc", dst="include/fmt", src="fmt/fmt")
+        self.copy("*.h", dst="include/cppformat", src="cppformat")
 
     def package_info(self):
-        self.cpp_info.libs = ["fmt"]
+        self.cpp_info.defines.append("FMT_HEADER_ONLY")
+
